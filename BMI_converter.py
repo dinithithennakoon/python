98293bf928c7e262112d0b97a61eb1310824bb4f
Kaushalya Dinithi <167504029+dinithithennakoon@users.noreply.github.com>
2024-12-08 15:14:33 +0530

    Update BMI_converter.py
    BMI converter

--- BMI_converter.py
+++ BMI_converter.py
@@ -1,1 +1,24 @@
-type(90)
+name = input("Enter you're name: ")
+weight = float(input("Enter weight in pounds: "))
+height = float(input("Enter height in inches: "))
+
+bmi = (weight * 703) / (height * height)
+print(name + "Your BMI: ", bmi)
+
+
+if bmi > 0:
+    if bmi < 18.5:
+        print(name ," You're Underweight")
+
+    elif bmi <=24.9 :
+         print(name,"You're Normal weight")
+    elif bmi <=29.9 :
+        print(name,"You're Overweight")
+    elif bmi <= 34.9:
+        print(name,"You're Obese")
+    elif bmi <= 39.9 :
+        print(name,"You're Severely Obese")
+    else:
+        print(name,"You're Morbidly Obese")
+else:
+    print("ERROR!")
